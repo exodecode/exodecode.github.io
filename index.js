a892e44d4521fafd523091c4ad82033ccb6c2ba0
Nicholas Lee <nicholasleexyz@gmail.com>
2023-08-06 03:49:11 -0600

    Added typescript for funzies

--- index.js
+++ index.js
@@ -1,165 +1,142 @@
-const songFileDir = './assets/music/';
-const songFileType = '.wav';
-
-const songFiles = [
-  'Cherry',
-  'Demyelination',
-  'Microlattice',
-  'OGLE-TR-122b',
-  'Ozone',
-  'WF6',
-  'Who_Let_This_Guy_in_Here!',
+var songFileDir = './assets/music/';
+var songFileType = '.wav';
+var songFiles = [
+    'Cherry',
+    'Demyelination',
+    'Microlattice',
+    'OGLE-TR-122b',
+    'Ozone',
+    'WF6',
+    'Who_Let_This_Guy_in_Here!',
 ];
-
 /* Stateful Variables */
-const play = document.getElementById('play');
-const pause = document.getElementById('pause');
-const next = document.getElementById('next');
-const before = document.getElementById('before');
-const songList = document.getElementById('song-list');
-const songImage = document.getElementById('song-image');
-const songTitle = document.getElementById('song-header');
-
-const audio = new Audio();
-let currentSong;
-let currentIndex = 0;
-
-const songElements = [];
-const songNames = [];
-const paths = [];
-
-let hasPlayed = false;
-
-/**
- * Utility function to replace all occurrences of a
- * string(search) with another string(replace) inside a given string and then
- * returns the result as a new string.
- * @param {string} string
- * @param {string} search
- * @param {string} replace
- * @return {string}
- */
-function replaceAll(string, search, replace) {
-  return string.split(search).join(replace);
+var play = document.getElementById('play');
+var pause = document.getElementById('pause');
+var next = document.getElementById('next');
+var before = document.getElementById('before');
+var songList = document.getElementById('song-list');
+var songTitle = document.getElementById('song-header');
+var songImage = document.getElementById('song-image');
+var audio = new Audio();
+var currentSong;
+var currentIndex = 0;
+var songElements = [];
+var songNames = [];
+var paths = [];
+var hasPlayed = false;
+function replaceAll(s, search, replace) {
+    return s.split(search).join(replace);
 }
-
 /**
- * Sets audio source to a path in paths array at index.
+ * Plays a song in paths array at a given index from the audio instance.
  * @param {number} index
+ * @returns {void}
  */
 function playAtIndex(index) {
-  currentIndex = index;
-  const currentPath = paths[currentIndex];
-
-  if (!audio.paused) {
-    audio.pause();
-    audio.currentTime = 0;
-  }
-
-  audio.src = currentPath;
-
-  currentSong.classList.remove('current-song');
-  currentSong = songElements[currentIndex];
-  currentSong.classList.add('current-song');
-
-  songTitle.innerText = songNames[index];
-
-  play.hidden = true;
-  pause.hidden = false;
-
-  audio.play();
-  hasPlayed = true;
-  songImage.src = './assets/jumpyBug.gif';
+    currentIndex = index;
+    var currentPath = paths[currentIndex];
+    if (!audio.paused) {
+        audio.pause();
+        audio.currentTime = 0;
+    }
+    audio.src = currentPath;
+    currentSong.classList.remove('current-song');
+    currentSong = songElements[currentIndex];
+    currentSong.classList.add('current-song');
+    if (songTitle !== null && play !== null && pause !== null && songImage !== null) {
+        songTitle.innerText = songNames[index];
+        play.hidden = true;
+        pause.hidden = false;
+        audio.play();
+        hasPlayed = true;
+        songImage.src = './assets/jumpyBug.gif';
+    }
 }
-
 /**
  * Toggles the current Audio instance between play and pause.
  */
 function togglePlay() {
-  if (!audio.paused) {
-    play.hidden = false;
-    pause.hidden = true;
-    audio.pause();
-    songImage.src = './assets/jumpyBug_paused.png';
-  } else {
-    play.hidden = true;
-    pause.hidden = false;
-    audio.play();
-    songImage.src = './assets/jumpyBug.gif';
-  }
+    if (songTitle !== null && play !== null && pause !== null && songImage !== null) {
+        if (!audio.paused) {
+            play.hidden = false;
+            pause.hidden = true;
+            audio.pause();
+            songImage.src = './assets/jumpyBug_paused.png';
+        }
+        else {
+            play.hidden = true;
+            pause.hidden = false;
+            audio.play();
+            songImage.src = './assets/jumpyBug.gif';
+        }
+    }
 }
-
 /**
  * Plays the song after the current index.
  */
 function playNext() {
-  playAtIndex((currentIndex + 1) % paths.length);
+    playAtIndex((currentIndex + 1) % paths.length);
 }
-
 /**
  * Plays the song before the current index.
  */
 function playPrevious() {
-  playAtIndex((currentIndex - 1 + paths.length) % paths.length);
+    playAtIndex((currentIndex - 1 + paths.length) % paths.length);
 }
-
-songFiles.forEach((file, i) => {
-  const songElem = document.createElement('span');
-  songElem.classList.add('song');
-  const songName = replaceAll(file, '_', ' ');
-
-  songNames.push(songName);
-
-  songElem.innerText = songName;
-  songList.appendChild(songElem);
-  songElements.push(songElem);
-
-  const path = songFileDir + file +songFileType;
-
-  paths.push(path);
-
-  songElem.onclick = () => {
-    playAtIndex(i);
-  };
-});
-
-currentSong = songElements[0];
-currentSong.classList.add('current-song');
-songTitle.innerText = replaceAll(currentSong.innerText, '_', ' ');
-
-audio.addEventListener('ended', playNext, false);
-
-next.addEventListener('click', playNext);
-before.addEventListener('click', playPrevious);
-
-play.addEventListener('click', () => {
-  play.hidden = true;
-  pause.hidden = false;
-  if (!hasPlayed) {
-    playAtIndex(currentIndex);
-  } else {
-    if (!audio.paused) {
-      audio.pause();
-      audio.currentTime = 0;
+/**
+ * Initialization
+ */
+function init() {
+    if (songList !== null && songTitle !== null && next !== null && before !== null && play !== null && pause !== null) {
+        songFiles.forEach(function (file, i) {
+            var songElem = document.createElement('span');
+            songElem.classList.add('song');
+            var songName = replaceAll(file, '_', ' ');
+            songNames.push(songName);
+            songElem.innerText = songName;
+            songList.appendChild(songElem);
+            songElements.push(songElem);
+            var path = songFileDir + file + songFileType;
+            paths.push(path);
+            songElem.onclick = function () {
+                playAtIndex(i);
+            };
+        });
+        currentSong = songElements[0];
+        currentSong.classList.add('current-song');
+        songTitle.innerText = replaceAll(currentSong.innerText, '_', ' ');
+        audio.addEventListener('ended', playNext, false);
+        next.addEventListener('click', playNext);
+        before.addEventListener('click', playPrevious);
+        play.addEventListener('click', function () {
+            play.hidden = true;
+            pause.hidden = false;
+            if (!hasPlayed) {
+                playAtIndex(currentIndex);
+            }
+            else {
+                if (!audio.paused) {
+                    audio.pause();
+                    audio.currentTime = 0;
+                }
+                audio.play();
+                songImage.src = './assets/jumpyBug.gif';
+            }
+        });
+        pause.addEventListener('click', function () {
+            play.hidden = false;
+            pause.hidden = true;
+            audio.pause();
+            songImage.src = './assets/jumpyBug_paused.png';
+        });
+        document.body.onkeydown = function (e) {
+            if (hasPlayed) {
+                if (e.keyCode == 32) {
+                    togglePlay();
+                }
+            }
+        };
+        audio.src = paths[0];
     }
-    audio.play();
-    songImage.src = './assets/jumpyBug.gif';
-  }
-});
-
-pause.addEventListener('click', () => {
-  play.hidden = false;
-  pause.hidden = true;
-  audio.pause();
-  songImage.src = './assets/jumpyBug_paused.png';
-});
-
-document.body.onkeydown = function(e) {
-  if (hasPlayed) {
-    if ( e.keyCode == 32 ) {
-      togglePlay();
-    }
-  }
-};
-
-audio.src = paths[0];
+}
+init();
---
+++ index.ts
@@ -0,0 +1,169 @@
+const songFileDir: string = './assets/music/';
+const songFileType: string = '.wav';
+
+const songFiles: string[] = [
+  'Cherry',
+  'Demyelination',
+  'Microlattice',
+  'OGLE-TR-122b',
+  'Ozone',
+  'WF6',
+  'Who_Let_This_Guy_in_Here!',
+];
+
+/* Stateful Variables */
+const play = document.getElementById('play');
+const pause = document.getElementById('pause');
+const next = document.getElementById('next');
+const before = document.getElementById('before');
+const songList = document.getElementById('song-list');
+const songTitle = document.getElementById('song-header');
+const songImage = <HTMLImageElement>document.getElementById('song-image');
+
+const audio = new Audio();
+let currentSong: HTMLElement;
+let currentIndex: number = 0;
+
+const songElements: HTMLElement[] = [];
+const songNames: string[] = [];
+const paths: string[] = [];
+
+let hasPlayed: boolean = false;
+
+function replaceAll(s: string, search: string, replace: string): string {
+  return s.split(search).join(replace);
+}
+
+/**
+ * Plays a song in paths array at a given index from the audio instance.
+ * @param {number} index 
+ * @returns {void}
+ */
+function playAtIndex(index: number): void {
+  currentIndex = index;
+  const currentPath = paths[currentIndex];
+
+  if (!audio.paused) {
+    audio.pause();
+    audio.currentTime = 0;
+  }
+
+  audio.src = currentPath;
+
+  currentSong.classList.remove('current-song');
+  currentSong = songElements[currentIndex];
+  currentSong.classList.add('current-song');
+
+  if (songTitle !== null && play !== null && pause !== null && songImage !== null) {
+    songTitle.innerText = songNames[index];
+    play.hidden = true;
+    pause.hidden = false;
+
+    audio.play();
+    hasPlayed = true;
+    songImage.src = './assets/jumpyBug.gif';
+  }
+}
+
+/**
+ * Toggles the current Audio instance between play and pause.
+ */
+function togglePlay(): void {
+  if (songTitle !== null && play !== null && pause !== null && songImage !== null) {
+    if (!audio.paused) {
+      play.hidden = false;
+      pause.hidden = true;
+      audio.pause();
+      songImage.src = './assets/jumpyBug_paused.png';
+    } else {
+      play.hidden = true;
+      pause.hidden = false;
+      audio.play();
+      songImage.src = './assets/jumpyBug.gif';
+    }
+  }
+}
+
+/**
+ * Plays the song after the current index.
+ */
+function playNext(): void {
+  playAtIndex((currentIndex + 1) % paths.length);
+}
+
+/**
+ * Plays the song before the current index.
+ */
+function playPrevious(): void {
+  playAtIndex((currentIndex - 1 + paths.length) % paths.length);
+}
+
+/**
+ * Initialization
+ */
+function init(): void {
+  if (songList !== null && songTitle !== null && next !== null && before !== null && play !== null && pause !== null) {
+    songFiles.forEach((file, i) => {
+      const songElem = document.createElement('span');
+      songElem.classList.add('song');
+      const songName = replaceAll(file, '_', ' ');
+
+      songNames.push(songName);
+
+      songElem.innerText = songName;
+      songList.appendChild(songElem);
+      songElements.push(songElem);
+
+      const path = songFileDir + file + songFileType;
+
+      paths.push(path);
+
+      songElem.onclick = () => {
+        playAtIndex(i);
+      };
+    });
+
+    currentSong = songElements[0];
+    currentSong.classList.add('current-song');
+    songTitle.innerText = replaceAll(currentSong.innerText, '_', ' ');
+
+    audio.addEventListener('ended', playNext, false);
+
+    next.addEventListener('click', playNext);
+    before.addEventListener('click', playPrevious);
+
+    play.addEventListener('click', () => {
+      play.hidden = true;
+      pause.hidden = false;
+      if (!hasPlayed) {
+        playAtIndex(currentIndex);
+      } else {
+        if (!audio.paused) {
+          audio.pause();
+          audio.currentTime = 0;
+        }
+        audio.play();
+        songImage.src = './assets/jumpyBug.gif';
+      }
+    });
+
+    pause.addEventListener('click', () => {
+      play.hidden = false;
+      pause.hidden = true;
+      audio.pause();
+      songImage.src = './assets/jumpyBug_paused.png';
+    });
+
+    document.body.onkeydown = function (e) {
+      if (hasPlayed) {
+        if (e.keyCode == 32) {
+          togglePlay();
+        }
+      }
+    };
+
+    audio.src = paths[0];
+  }
+}
+
+init();
