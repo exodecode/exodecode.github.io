7996c092fd64a612f38b850ab7273a1b4f290a0b
Nicholas Lee <nicholasleexyz@gmail.com>
2023-08-18 01:16:42 -0600

    Assets are now hosted externally

--- src/Player.jsx
+++ src/Player.jsx
@@ -1,6 +1,12 @@
 import { PropTypes } from "prop-types";
 
 function Player( { songs, isPlaying, setIsPlaying, currentSong, setCurrentSong, audioElem } ) {
+  const playingJumpyUrl = "https://docs.google.com/uc?export=download&id=1Yc5dkS4E7qTRx-rcISx_ciirSmY6kt6p";
+  const pausedJumpyUrl = "https://docs.google.com/uc?export=download&id=1wahHrMOjedBAw8MmqXFRT0IRCm9im2cA";
+  const previousIcon = "https://docs.google.com/uc?export=download&id=1w8kdI9eARPTTvCrMkOm0mPBdY6KYDv-s";
+  const nextIcon = "https://docs.google.com/uc?export=download&id=1_rZIHjFkcIm4XNwUvFjogTtcvhJ640hQ";
+  const playIcon = "https://docs.google.com/uc?export=download&id=1Ek5wF1n1Tu7InTEtmlbk3JHMD9aKvA5p";
+  const pauseIcon = "https://docs.google.com/uc?export=download&id=16dw5Bk2kHVP0q462GrLRW9a0006_Y5DS";
 
   function setCurrentTo(index){
     let song = songs[index];
@@ -26,7 +32,6 @@ function Player( { songs, isPlaying, setIsPlaying, currentSong, setCurrentSong, 
 
   function previous(){
     let index = (songs.indexOf(currentSong) - 1 + songs.length) % songs.length;
-    console.log("current index: " + index);
 
     setCurrentTo(index);
   }
@@ -42,13 +47,13 @@ function Player( { songs, isPlaying, setIsPlaying, currentSong, setCurrentSong, 
         <div className="list-container">
             <div className="player" >
                 <h1 className="song-header">{currentSong.title}</h1>
-                <img src={isPlaying ? "/assets/jumpyBug.gif" : "/assets/jumpyBug_paused.png"} alt="" className="song-image"/>
+                <img src={isPlaying ? playingJumpyUrl : pausedJumpyUrl} alt="" className="song-image"/>
 
                 <div className="song-controls">
-                    <img src="/assets/navigate_before_FILL0_wght400_GRAD0_opsz48.svg" alt="" className="song-control-img" onClick={previous} />
-                    <img src="/assets/play_circle_FILL0_wght400_GRAD0_opsz48.svg" alt="" className="song-control-img" hidden={isPlaying} onClick={() => setIsPlaying(true)}/>
-                    <img src="/assets/pause_circle_FILL0_wght400_GRAD0_opsz48.svg" alt="" className="song-control-img" hidden={!isPlaying} onClick={() => setIsPlaying(false)}/>
-                    <img src="/assets/navigate_next_FILL0_wght400_GRAD0_opsz48.svg" alt="" className="song-control-img" onClick={next} />
+                    <img src={previousIcon} alt="" className="song-control-img" onClick={previous} />
+                    <img src={playIcon} alt="" className="song-control-img" hidden={isPlaying} onClick={() => setIsPlaying(true)}/>
+                    <img src={pauseIcon} alt="" className="song-control-img" hidden={!isPlaying} onClick={() => setIsPlaying(false)}/>
+                    <img src={nextIcon} alt="" className="song-control-img" onClick={next} />
                 </div>
             </div>
 
@@ -62,13 +67,13 @@ function Player( { songs, isPlaying, setIsPlaying, currentSong, setCurrentSong, 
 }
 
 Player.propTypes = {
- songs: PropTypes.arrayOf(PropTypes.shape({title: PropTypes.string, source: PropTypes.string})),
- setSongs: PropTypes.func,
- isPlaying: PropTypes.bool,
- setIsPlaying: PropTypes.func,
- currentSong: PropTypes.shape({title: PropTypes.string, source: PropTypes.string}),
- setCurrentSong: PropTypes.func,
- audioElem: PropTypes.object
+  songs: PropTypes.arrayOf(PropTypes.shape({title: PropTypes.string, source: PropTypes.string})),
+  setSongs: PropTypes.func,
+  isPlaying: PropTypes.bool,
+  setIsPlaying: PropTypes.func,
+  currentSong: PropTypes.shape({title: PropTypes.string, source: PropTypes.string}),
+  setCurrentSong: PropTypes.func,
+  audioElem: PropTypes.object
 }
 
 export default Player
